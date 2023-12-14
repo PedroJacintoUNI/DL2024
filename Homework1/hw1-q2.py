6cc7925f1a1b5b3ae9239f87bfa07185b94fc15a
PedroJacintoUNI <pedro1jacinto.uni@gmail.com>
2023-12-14 11:26:17 +0000

    Q2 default

--- Homework1/hw1-q2.py
+++ Homework1/hw1-q2.py
@@ -187,9 +187,9 @@ def main():
                         help="Size of training batch.")
     parser.add_argument('-learning_rate', type=float, default=0.001)
     parser.add_argument('-l2_decay', type=float, default=0)
-    parser.add_argument('-hidden_size', type=int, default=100)
-    parser.add_argument('-layers', type=int, default=1)
-    parser.add_argument('-dropout', type=float, default=0.3)
+    parser.add_argument('-hidden_size', type=int, default=200)
+    parser.add_argument('-layers', type=int, default=2)
+    parser.add_argument('-dropout', type=float, default=0)
     parser.add_argument('-activation',
                         choices=['tanh', 'relu'], default='relu')
     parser.add_argument('-optimizer',
